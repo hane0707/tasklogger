8384711ebe5fa9975058f91d9186c1da16cb52b6
koob <hidamari4256@gmail.com>
2021-06-13 15:02:28 +0900

    エラー時にメッセージを表示するように修正

--- nuxt/frontend/pages/task/index.vue
+++ nuxt/frontend/pages/task/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="row">
     <div class="col">
-      <div class="row mb-4">
+      <b-alert v-model="errorFlg" variant="danger" dismissible>
+        {{ errorMessage }}
+      </b-alert>
+      <div class="row task-regist mb-4">
         <input
           id="task-title"
           v-model="newTask.title"
@@ -9,31 +12,33 @@
           class="form-control col-sm-4"
           placeholder="タスクのタイトル">
         <button @click="registTask(newTask, searchDate)" class="btn btn-success col-sm-2 mx-2 my-sm-0">新しい作業を開始する</button>
-        </div>
-      <div class="row mb-4">
-        <input type="date" v-model="searchDate" class="col-sm-2 form-control">
-        <button @click="getList(searchDate)" class="btn btn-primary mx-2">検索</button>
-        {{ count }} 件の履歴があります。
       </div>
-      <div v-for="task in viewList" :key="task.id">
-        <div class="mb-4">
-          <!-- TaskCard Start-->
-          <div class="card task-card">
-            <div class="card-body">
-              <p><b>タイトル：</b>{{ task.title }}</p>
-              <div>
-                <p><b>詳細：</b></p>
-                <p class="ml-4">{{ task.details }}</p>
+      <div class="task-search">
+        <div class="row mb-4">
+          <input type="date" v-model="searchDate" class="col-sm-2 form-control">
+          <button @click="getList(searchDate)" class="btn btn-primary mx-2">検索</button>
+          {{ count }} 件の履歴があります。
+        </div>
+        <div v-for="task in viewList" :key="task.id">
+          <div class="mb-4">
+            <!-- TaskCard Start-->
+            <div class="card task-card">
+              <div class="card-body">
+                <p><b>タイトル：</b>{{ task.title }}</p>
+                <div>
+                  <p><b>詳細：</b></p>
+                  <p class="ml-4">{{ task.details }}</p>
+                </div>
+                <p><b>開始日時：</b>{{ task.start_date }} {{ task.start_time }}</p>
+                <p><b>終了日時：</b>{{ task.end_date }} {{ task.end_time }}</p>
+                <nuxt-link :to="`/task/${task.id}/edit/`" class="btn btn-sm btn-outline-primary">
+                  編集
+                </nuxt-link>
+                <button @click="deleteTask(task.id, searchDate)" class="btn btn-sm btn-outline-danger">削除</button>
               </div>
-              <p><b>開始日時：</b>{{ task.start_date }} {{ task.start_time }}</p>
-              <p><b>終了日時：</b>{{ task.end_date }} {{ task.end_time }}</p>
-              <nuxt-link :to="`/task/${task.id}/edit/`" class="btn btn-sm btn-outline-primary">
-                編集
-              </nuxt-link>
-              <button @click="deleteTask(task.id, searchDate)" class="btn btn-sm btn-outline-danger">削除</button>
             </div>
+            <!-- TaskCard End -->
           </div>
-          <!-- TaskCard End -->
         </div>
       </div>
     </div>
@@ -81,7 +86,9 @@ export default {
     ...mapState({
       list: state => state.task.list,
       viewList: state => state.task.viewList,
-      count: state => state.task.count
+      count: state => state.task.count,
+      errorFlg: state => state.task.errorFlg,
+      errorMessage: state => state.task.errorMessage
     })
   },
   filters: {
--- nuxt/frontend/store/task.js
+++ nuxt/frontend/store/task.js
@@ -1,18 +1,18 @@
 export const state = () => ({
   list: [],
   viewList: [],
-  count: 0
+  count: 0,
+  errorFlg: false,
+  errorMessage: ''
 })
 
 export const mutations = {
   setList: (state, list) => (state.list = list),
   setViewList: (state, viewList) => (state.viewList = viewList),
   setCount: (state, count) => (state.count = count),
-  removeTask: (state, id) => (state.viewList = state.viewList.filter(task => task.id !== id)),
-  setRegistTask: (state, task) => {
-    state.list.unshift(task)
-    // state.list.unshift(task)
-  }
+  setErrorFlg: (state, flg) => (state.errorFlg = flg),
+  setErrorMessage: (state, message) => (state.errorMessage = message),
+  removeTask: (state, id) => (state.viewList = state.viewList.filter(task => task.id !== id))
 }
 
 export const actions = {
@@ -37,14 +37,18 @@ export const actions = {
       console.log(e)
     }
   },
-  async registTask ({ dispatch }, { newTask, date }) {
+  async registTask ({ dispatch, commit }, { newTask, date }) {
+    // タイトルがない場合は"no title"を設定
     newTask.title = newTask.title === '' ? 'no title' : newTask.title
 
     let response = ''
     try {
       response = await this.$axios.$post('/api/task/', newTask) // eslint-disable-line
     } catch (error) {
+      console.log('エラー発生：')
       console.log(error.response)
+      commit('setErrorFlg', true)
+      commit('setErrorMessage', 'データ登録時にエラーが発生しました。')
     }
 
     if (response.start_date === date) {
